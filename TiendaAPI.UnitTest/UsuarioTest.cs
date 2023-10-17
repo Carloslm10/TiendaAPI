705a79d115a565d004033c10720e135fd3c3effd
carlos hernandez <ch335037@gmail.com>
2023-10-16 18:50:42 -0600

    Actualizacion

---
+++ TiendaAPI.UnitTest/UsuarioTest.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TiendaAPI.Context;
+using TiendaAPI.DTOs;
+using TiendaAPI.Mappings;
+using TiendaAPI.Respositories;
+using TiendaAPI.Settings;
+
+namespace TiendaAPI.UnitTest
+{
+    public class UsuarioTest
+    {
+        
+    }
+}
